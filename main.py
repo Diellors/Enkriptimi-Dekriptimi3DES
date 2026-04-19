9e33ef608206bce8f8a4eeb94240e1835ff772fa
Diellors <diellor.sinani@student.uni-pr.edu>
2026-04-19 09:22:53 +0200

    Shtimi i qelesit (key)

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from Crypto.Cipher import DES3
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
 import base64
+
+KEY = b"1234567890abcdef12345678"
+KEY = DES3.adjust_key_parity(KEY)
